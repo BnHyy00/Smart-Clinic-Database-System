172405e297185239733590b29fde2fdec4a9376b
Raneem-ar <s250015298@seu.edu.sa>
2026-07-24 22:28:18 +0300

    Create Medicines table with specified columns

--- 03_Medicines_Operations_Trigger.sql
+++ 03_Medicines_Operations_Trigger.sql
@@ -1,1 +1,9 @@
+DROP TABLE IF EXISTS Medicines;
+ CREATE TABLE Medicines (
+  Medicine_ID INT PRIMARY KEY,
+  Medicine_Name VARCHAR(100) NOT NULL,
+  Category VARCHAR(50),
+  Unit_Price DECIMAL(10, 2) NOT NULL,
+  Stock_Quantity INT NOT NULL
+);
 
