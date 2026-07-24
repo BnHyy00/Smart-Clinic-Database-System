011a9e8dd71f82ed327d3da8473311d1c0dcedbf
BN <albndary.aalhayaf@gmail.com>
2026-07-24 20:30:11 +0300

    Create Payments table structure

--- SmartClinic.sql
+++ SmartClinic.sql
@@ -9,3 +9,13 @@
 
 
 -- Student 4: Payments, SELECT, and VIEW
+USE SmartClinicDB;
+
+CREATE TABLE Payments (
+    payment_ID INT AUTO_INCREMENT PRIMARY KEY,
+    appointment_ID INT NOT NULL,
+    payment_date DATE NOT NULL,
+    amount DECIMAL(10,2) NOT NULL,
+    payment_method VARCHAR(30) NOT NULL,
+    payment_status VARCHAR(20) NOT NULL
+);
