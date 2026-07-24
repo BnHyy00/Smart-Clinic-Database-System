0b1f6f76cc55853e4e1c0bec8da9afd2487570f7
Ebtehal <121483213+Ebtehal-mn@users.noreply.github.com>
2026-07-24 19:09:19 +0300

    Initialize SmartClinicDB and create tables
    Created SmartClinicDB with Doctors and Patients tables.

--- 01_Patients_Doctors.sql
+++ 01_Patients_Doctors.sql
@@ -1,1 +1,24 @@
+CREATE DATABASE SmartClinicDB;
 
+/*-------------------------------------
+--Here is the Doctors table creation
+-------------------------------------*/
+CREATE TABLE Doctors (doctor_ID INT AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  date_of_birth DATE NOT NULL,
+  gender ENUM('Male', 'Female') NOT NULL,
+  specialization VARCHAR(80) NOT NULL,
+  phone_number VARCHAR(15) UNIQUE NOT NULL,
+  email VARCHAR(80) UNIQUE NOT NULL);
+/*-------------------------------------
+--Here is the Patients table creation
+-------------------------------------*/
+CREATE TABLE Patients (patient_ID INT AUTO_INCREMENT  PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    gender ENUM('Male', 'Female') NOT NULL,
+    phone_number VARCHAR(15) UNIQUE NOT NULL,
+    email VARCHAR(80) UNIQUE,
+    address VARCHAR(255));
