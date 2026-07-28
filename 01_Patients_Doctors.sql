2ddac2401e088cbad444eb766f7735288cbe686f
Ebtehal <121483213+Ebtehal-mn@users.noreply.github.com>
2026-07-28 20:36:47 +0300

    updated Prescriptions table and initiated Prescription_Medicines table
    Edited Prescriptions due to crating Prescription_Medicines as a result of many-to-many relationship between Prescription Medicines tables

--- 01_Patients_Doctors.sql
+++ 01_Patients_Doctors.sql
@@ -1,7 +1,5 @@
 CREATE DATABASE SmartClinicDB;
-/*-------------------------------------
---Here is the Doctors table creation
--------------------------------------*/
+/*---------Doctors table creation*/
 CREATE TABLE Doctors (doctor_ID INT AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
@@ -11,9 +9,7 @@ CREATE TABLE Doctors (doctor_ID INT AUTO_INCREMENT PRIMARY KEY,
   phone_number VARCHAR(15) UNIQUE NOT NULL,
   email VARCHAR(80) UNIQUE NOT NULL);
 
-/*-------------------------------------
---Here is the Patients table creation
--------------------------------------*/
+/*---------Patients table creation*/
 CREATE TABLE Patients (patient_ID INT AUTO_INCREMENT  PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
@@ -22,49 +18,58 @@ CREATE TABLE Patients (patient_ID INT AUTO_INCREMENT  PRIMARY KEY,
     phone_number VARCHAR(15) UNIQUE NOT NULL,
     email VARCHAR(80) UNIQUE,
     address VARCHAR(255));
-/*-------------------------------------
---Here is the Prescriptions table creation
--------------------------------------*/
+
+/*---------Prescriptions table creation*/
 CREATE TABLE Prescriptions (prescription_ID INT AUTO_INCREMENT  PRIMARY KEY,
-  patient_ID INT,
-  medicine_ID INT,
-  doctor_ID INT,
-  dosage VARCHAR(100) NOT NULL,
+  patient_ID INT NOT NULL,
+  doctor_ID INT NOT NULL,
   prescription_date DATE NOT NULL,
   FOREIGN KEY (patient_ID) REFERENCES Patients(patient_ID),
-  FOREIGN KEY (doctor_ID) REFERENCES Doctors(doctor_ID),
-  FOREIGN KEY (medicine_ID) REFERENCES Medicines(medicine_ID)
-);
+  FOREIGN KEY (doctor_ID) REFERENCES Doctors(doctor_ID));
+
+/*---------Prescription_Medicines table creation*/
+CREATE TABLE Prescription_Medicines (prescription_ID INT,
+  Medicine_ID INT,
+  dosage VARCHAR(100) NOT NULL,
+  PRIMARY KEY (prescription_ID, Medicine_ID),
+  FOREIGN KEY (prescription_ID) REFERENCES Prescriptions(prescription_ID) ON DELETE CASCADE,
+  FOREIGN KEY(Medicine_ID) REFERENCES Medicines(Medicine_ID));
 
 /*-------------------------------------
---Here to Insert into Doctors table
+-----------Insertions into tables
 -------------------------------------*/
+
+/*---------Insert into Doctors table*/
 INSERT INTO Doctors (first_name, last_name,date_of_birth, gender, specialization, phone_number, email) VALUES
 ('Ahmed', 'Ali', '1980-05-10', 'Male', 'Cardiology', '0501112233', 'ahmed.otaibi@clinic.com'),
 ('Fatima', 'Husain', '1985-08-15', 'Female', 'Pediatrics', '0552223344', 'fatima.harbi@clinic.com'),
 ('Mohammed', 'Salem', '1978-12-01', 'Male', 'Dermatology', '0563334455', 'mohammed.salem@clinic.com'),
 ('Sara', 'Mohammed', '1990-03-22', 'Female', 'General Practice', '0544445566', 'sara.qahtani@clinic.com'),
 ('Rakan', 'Saud', '1982-11-05', 'Male', 'Orthopedics', '0535556677', 'khalid.sultan@clinic.com');
-/*-------------------------------------
---Here to Insert into Patients table
--------------------------------------*/
+
+/*---------Insert into Patients table*/
 INSERT INTO Patients (first_name, last_name,date_of_birth, gender, phone_number, email, address) VALUES
 ('Noura', 'Abdullah', '1995-04-12', 'Female', '0509998877', 'noura@email.com', 'Riyadh - An Nafal'),
 ('Abdullah', 'Nasser', '1988-09-23', 'Male', '0558887766', 'abdullah@email.com', 'Riyadh - Al-Falah'),
 ('Reem', 'Fahad', '2001-01-15', 'Female', '0567776655', 'reem@email.com', 'Riyadh - Al-Narjis'),
 ('Mohammed', 'Mansour', '1975-11-30', 'Male', '0546665544', '', 'Riyadh - An nada'),
 ('Layan', 'Tariq', '1999-06-05', 'Female', '0535554433', 'layan@email.com', 'Riyadh - Al-Yasmin');
-/*-------------------------------------
---Here to Insert into Prescriptions table
--------------------------------------*/
-INSERT INTO Prescriptions (patient_ID, medicine_ID, doctor_ID, dosage, prescription_date) VALUES
-(1, 1, 4,'1 tablet every 8 hours', '2026-03-01'), 
-(1, 3, 4,'1 tablet after meals', '2026-03-01'),    
-(2, 2, 5,'1 capsule twice daily', '2026-03-02'),    
-(3, 4, 3,'1 tablet before breakfast', '2026-03-03');
+
+/*---------Insert into Prescriptions table*/
+INSERT INTO Prescriptions (prescription_ID, patient_ID, doctor_ID, prescription_date) VALUES
+(1, 1, 4, '2026-03-01'),  
+(2, 2, 5, '2026-03-02'),  
+(3, 3, 3, '2026-03-03');
+
+/*---------Insert into Prescription_Medicines table*/
+INSERT INTO Prescription_Medicines (prescription_ID, Medicine_ID, dosage) VALUES
+(1, 1, '1 tablet every 8 hours'),       
+(1, 3, '1 tablet after meals'),         
+(2, 2, '1 capsule twice daily'),         
+(3, 4, '1 tablet before breakfast');
 
 /*------------------------------------------
---Here some SELECT queries for data retrival 
+--SELECT queries for data retrival 
 ------------------------------------------*/
 SELECT * FROM Doctors;
 SELECT patient_ID, first_name, last_name, gender, phone_number FROM Patients;
