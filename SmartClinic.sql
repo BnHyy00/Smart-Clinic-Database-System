13b70045aeff22e4904f7668e6489f7538ed0ec0
bashaer1341-eng <besho1341@gmail.com>
2026-08-05 02:00:50 +0300

    Add Appointments table with foreign keys

--- SmartClinic.sql
+++ SmartClinic.sql
@@ -47,7 +47,16 @@ CREATE TABLE Appointments (
 -- Student 2: Treatments table
 -------------------------------------*/
 
-
+CREATE TABLE Appointments (
+    appointment_ID INT AUTO_INCREMENT PRIMARY KEY,
+    patient_ID INT NOT NULL,
+    doctor_ID INT NOT NULL,
+    appointment_date DATE NOT NULL,
+    appointment_time TIME NOT NULL,
+    appointment_status VARCHAR(20) NOT NULL,
+    FOREIGN KEY (patient_ID) REFERENCES Patients(patient_ID) ON DELETE CASCADE,
+    FOREIGN KEY (doctor_ID) REFERENCES Doctors(doctor_ID) ON DELETE CASCADE
+);
 /*-------------------------------------
 -- Student 3: Medicines table
 -------------------------------------*/
